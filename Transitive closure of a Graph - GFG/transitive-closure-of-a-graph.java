1145709d42c1c10de9e02ec8176e53bf2bee4124
manoj1698 <97618377+manoj1698@users.noreply.github.com>
2023-10-17 22:21:51 +0530

    Added solution - LeetHub

---
+++ Transitive closure of a Graph - GFG/transitive-closure-of-a-graph.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int N = Integer.parseInt(in.readLine());
+            int graph[][] = new int[N][N];
+            
+            for(int i = 0;i < N;i++)
+                {String a[] = in.readLine().trim().split("\\s+");
+                for(int j=0;j<N;j++)
+                graph[i][j] = Integer.parseInt(a[j]);}
+            
+            Solution ob = new Solution();
+            ArrayList<ArrayList<Integer>> ans = ob.transitiveClosure(N, graph);
+            for(int i = 0;i < N;i++)
+               { for(int j = 0;j < ans.get(i).size();j++)
+                    System.out.print(ans.get(i).get(j) + " ");
+            System.out.println();}
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static ArrayList<ArrayList<Integer>> transitiveClosure(int N, int graph[][])
+    {
+        // code here
+        for(int k=0;k<graph.length;k++) {
+			for(int i=0;i<graph.length;i++) {
+				for(int j=0;j<graph.length;j++) {
+					if(i == j) {
+						graph[i][j] = 1;
+					}
+					if((graph[i][k]==1 && graph[k][j]==1) && graph[i][j] == 0) {
+						graph[i][j] = 1;
+					}
+				}
+			}
+		}
+		ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+		ArrayList<Integer> list ;
+		for(int i=0;i<graph.length;i++) {
+			list = new ArrayList<>();
+			for(int j=0;j<graph[0].length;j++) {
+				list.add(graph[i][j]);
+			}
+			ans.add(list);
+		}
+		return ans;
+    }
+}
